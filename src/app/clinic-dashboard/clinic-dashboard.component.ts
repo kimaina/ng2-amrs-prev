a3658e602475031efc46a623b3e9ff49de3b61f4
Emmanuel Nyachoke <achachiez@gmail.com>
2016-09-28 16:00:21 +0300

    Update Routing

--- src/app/clinic-dashboard/clinic-dashboard.component.ts
+++ src/app/clinic-dashboard/clinic-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clinic-dashboard',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clinic-dashboard.component.css']
 })
 export class ClinicDashboardComponent implements OnInit {
-
-  constructor() { }
+  locationUuid: string;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      let locationUuid = params['location_uuid'];
+      if (locationUuid) {
+        console.log('Set selected location in service updated instance variable for locationUuid')
+        this.locationUuid = locationUuid;
+      } else {
+        console.log('Try get default location or use dropdown then redirect with the new uuid');
+      }
+    });
   }
 
 }
